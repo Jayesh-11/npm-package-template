8197239c6d1c47a0155c31d3171a5383b37251a0
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2023-04-16 12:03:39 +0530

    added more customization and moved out from React.FC

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
 interface ButtonProps {
-  label: string;
-  color: string;
+  children: React.ReactNode;
+  color?: string;
+  size?: 'small' | 'medium' | 'large';
   onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, color, onClick }) => (
-  <button style={{ backgroundColor: color }} onClick={onClick}>
-    {label}
-  </button>
-);
+function Button({ children, color = 'blue', size = 'medium', onClick }: ButtonProps) {
+  const styles = {
+    backgroundColor: color,
+    color: 'white',
+    padding: size === 'small' ? '0.5rem 1rem' : size === 'medium' ? '1rem 2rem' : '1.5rem 3rem',
+  };
+
+  return (
+    <button onClick={onClick} style={styles}>
+      {children}
+    </button>
+  );
+}
 
 export default Button;
